f2a39ebbc8bef7bb66319b145afbfcee65eef74d
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-09-15 16:14:52 -0400

    Initial Commit

---
+++ Navy Dolphin/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Navy Dolphin
+//
+//  Created by Ujjwal Singhania on 9/15/17.
+//  Copyright © 2017 Ujjwal Singhania. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
