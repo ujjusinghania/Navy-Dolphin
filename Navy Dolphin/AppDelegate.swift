598bc48c37e4f06c4457c5794e8ba2f0f4129732
Ujjwal Singhania <ujjwalsinghania@gmail.com>
2017-11-28 18:04:56 -0500

    Added some new graphics
    - Trashed old backgrounds for tasks.
    - Replaced with copyright free backgrounds to ensure fair use of graphics.

--- Navy Dolphin/AppDelegate.swift
+++ Navy Dolphin/AppDelegate.swift
@@ -19,7 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         {
             UserDefaults.standard.set(Array(repeating: false, count: TaskConstants.tasks.count), forKey: "checkboxTrackerArray")
             UserDefaults.standard.set(true, forKey: "HasLaunchedOnce")
-            UserDefaults.standard.synchronize()
         }
         return true
     }
@@ -32,6 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        UserDefaults.standard.synchronize()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -44,6 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        UserDefaults.standard.synchronize()
     }
 
 
